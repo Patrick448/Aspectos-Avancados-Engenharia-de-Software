84d02932571cc84e42399a77d25effbd87acd3b6
Patrick Carvalho <patrickcarvalho448@gmail.com>
2023-08-31 17:09:16 -0300

    testes

--- src/main/java/Refactoring/Loteria.java
+++ src/main/java/Refactoring/Loteria.java
@@ -17,17 +17,33 @@ public class Loteria {
 
     private static final int MAX_BET_SIZE = 15;
 
-    public List<Integer> filterNumbers(List<Integer> selectedNums){
+    private static final int MIN_NUMBER = 1;
+
+    private static final int MAX_NUMBER = 60;
+
+    private static final int TOTAL_RANDOM_NUMBERS = 6;
+
+    public List<Integer> verifySelectedNumbers(List<Integer> selectedNums){
         List<Integer> filteredNums = new ArrayList<>();
         for (Integer n: selectedNums){
-            if (n >= 1 && n <= 60 && !filteredNums.contains(n)){
+            if (n < MIN_NUMBER || n > MAX_NUMBER){
+                throw new IllegalArgumentException("a lista deve conter números entre 1 e 60");
+            }
+            if (filteredNums.contains(n)){
+                throw new IllegalArgumentException("a lista não pode conter números repetidos");
+            }else{
                 filteredNums.add(n);
             }
+
         }
         return filteredNums;
     }
 
     public int countMatches(List<Integer> listA, List<Integer> listB){
+        if(listA == null || listB == null){
+            throw new IllegalArgumentException("lista não pode ser nula");
+        }
+
         int totalMatches =0;
         for (Integer i: listA){
             if (listB.contains(i)){
@@ -41,8 +57,8 @@ public class Loteria {
         List<Integer> randomNums = new ArrayList<>();
         int randomInt;
 
-        while (randomNums.size() < 6) {
-            randomInt = new Random().nextInt(59) + 1;
+        while (randomNums.size() < TOTAL_RANDOM_NUMBERS) {
+            randomInt = new Random().nextInt(MAX_NUMBER-1) + 1;
             if (!randomNums.contains(randomInt)) {
                 randomNums.add(randomInt);
             }
@@ -50,21 +66,33 @@ public class Loteria {
         return randomNums;
     }
 
-    public double makeBet(List<Integer> selectedNums, double prizeBaseValue) {
-        List<Integer> filteredNums = filterNumbers(selectedNums);
+    public double calculateFinalPrize(double prizeBaseValue, int totalMatches){
         double prizeValue = 0.0;
 
-        if (filteredNums.size() >= MIN_BET_SIZE && filteredNums.size() <= MAX_BET_SIZE) {
-            List<Integer> randomNums = randomIntListNoRepeat();
-            int totalMatches = countMatches(randomNums, filteredNums);
+        if (totalMatches == FIRST_PRIZE_MATCH_NUMBER){
+            prizeValue = prizeBaseValue;
+        } else if (totalMatches == SECOND_PRIZE_MATCH_NUMBER){
+            prizeValue = prizeBaseValue * SECOND_PLACE_PRIZE_FRACTION;
+        } else if (totalMatches == THIRD_PRIZE_MATCH_NUMBER){
+            prizeValue = prizeBaseValue * THIRD_PLACE_PRIZE_FRACTION;
+        }
 
-            if (totalMatches == FIRST_PRIZE_MATCH_NUMBER){
-                prizeValue = prizeBaseValue;
-            } else if (totalMatches == SECOND_PRIZE_MATCH_NUMBER){
-                prizeValue = prizeBaseValue * SECOND_PLACE_PRIZE_FRACTION;
-            } else if (totalMatches == THIRD_PRIZE_MATCH_NUMBER){
-                prizeValue = prizeBaseValue * THIRD_PLACE_PRIZE_FRACTION;
-            }
+        return prizeValue;
+    }
+
+    public double makeBet(List<Integer> selectedNums, List<Integer> randomNums,  double prizeBaseValue) {
+        if(selectedNums == null || randomNums == null){
+            throw new IllegalArgumentException("lista não pode ser nula");
+        }
+
+        double prizeValue = 0.0;
+        List<Integer> filteredNums = verifySelectedNumbers(selectedNums);
+
+        if (filteredNums.size() >= MIN_BET_SIZE && filteredNums.size() <= MAX_BET_SIZE) {
+            int totalMatches = countMatches(randomNums, filteredNums);
+            prizeValue = calculateFinalPrize(prizeBaseValue, totalMatches);
+        }else{
+            throw new IllegalArgumentException("lista de números deve ter entre 6 e 15 elementos");
         }
 
         return prizeValue;
---
+++ src/test/java/Refactoring/LoteriaTest.java
@@ -0,0 +1,193 @@
+package Refactoring;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoteriaTest {
+
+    Loteria loteria;
+    @BeforeEach
+    public void init(){
+        loteria = new Loteria();
+    }
+
+
+    @Test
+    public void deveRetornarExceçãoListaVerificada(){
+        List<Integer> list ;
+        list = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        List<Integer> filteredList = loteria.verifySelectedNumbers(list);
+        assertEquals(list, filteredList);
+
+    }
+
+    @Test
+    public void deveRetornarExceçãoListaNumerosRepetidos(){
+        List<Integer> list ;
+        list = Arrays.asList(1, 2, 3, 4, 5, 1);
+
+        try {
+            List<Integer> filteredList = loteria.verifySelectedNumbers(list);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("a lista não pode conter números repetidos", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveRetornarExceçãoNumeroMenorQue1(){
+        List<Integer> list ;
+        list = Arrays.asList(0, 1, 2, 3, 4, 5);
+
+        try {
+            List<Integer> filteredList = loteria.verifySelectedNumbers(list);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("a lista deve conter números entre 1 e 60", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveRetornarExceçãoNumeroMaioQue60(){
+        List<Integer> list ;
+        list = Arrays.asList(0, 61, 2, 3, 4, 5);
+
+        try {
+            List<Integer> filteredList = loteria.verifySelectedNumbers(list);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("a lista deve conter números entre 1 e 60", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornar0Acertos() {
+        List<Integer> aposta = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        List<Integer> sorteio = Arrays.asList(13, 14, 15, 16, 17, 18);
+
+        int acertos = loteria.countMatches(aposta, sorteio);
+        assertEquals(0, acertos);
+    }
+
+    @Test
+    void deveRetornar6Acertos(){
+        List<Integer> aposta = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        List<Integer> sorteio = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        int acertos = loteria.countMatches(aposta, sorteio);
+        assertEquals(6, acertos);
+    }
+
+    @Test
+    void deveRetornarErroListaNulaCountMatches1() {
+        List<Integer> listB = Arrays.asList(13, 14, 15, 16, 17, 18);
+        try {
+            loteria.countMatches(null, listB);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("lista não pode ser nula", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarErroListaNulaCountMatches2() {
+        List<Integer> listA = Arrays.asList(13, 14, 15, 16, 17, 18);
+        try {
+            loteria.countMatches(listA, null);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("lista não pode ser nula", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarPremioInteiro() {
+        double prize = loteria.calculateFinalPrize(100.0, 6);
+        assertEquals(100.0, prize);
+    }
+
+    @Test
+    void deveRetornarPremioSegundo() {
+        double prize = loteria.calculateFinalPrize(100.0, 5);
+        assertEquals(20.0, prize);
+    }
+
+    @Test
+    void deveRetornarPremioTerceiro() {
+        double prize = loteria.calculateFinalPrize(100.0, 4);
+        assertEquals(5.0, prize);
+    }
+
+    @Test
+    void deveRetornarPremioZero() {
+        double prize = loteria.calculateFinalPrize(100.0, 3);
+        assertEquals(0.0, prize);
+    }
+
+    @Test
+    void deveRetornarErroApostaNula() {
+
+        List<Integer> aposta = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        List<Integer> sorteio = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+
+        try{
+            loteria.makeBet(null, sorteio, 100.0);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("lista não pode ser nula", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarErroSorteioNulo() {
+
+        List<Integer> aposta = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        List<Integer> sorteio = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+
+        try{
+            loteria.makeBet(aposta, null, 100.0);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("lista não pode ser nula", e.getMessage());
+        }
+    }
+
+    @Test
+    void deveRetornarErroApostaPequena() {
+
+        List<Integer> aposta = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> sorteio = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+
+        try{
+            loteria.makeBet(aposta, sorteio, 100.0);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("lista de números deve ter entre 6 e 15 elementos", e.getMessage());
+        }
+    }
+
+
+    @Test
+    void deveRetornarErroApostaGrande() {
+
+        List<Integer> aposta = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16);
+        List<Integer> sorteio = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+
+        try{
+            loteria.makeBet(aposta, sorteio, 100.0);
+            fail();
+        }catch (IllegalArgumentException e){
+            assertEquals("lista de números deve ter entre 6 e 15 elementos", e.getMessage());
+        }
+    }
+}
